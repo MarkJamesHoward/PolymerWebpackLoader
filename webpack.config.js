ea390789d9e1aca8ae0b4048bd1fe873775a0749
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2017-07-18 19:25:21 +1200

    ignore added

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
     entry: './app.js',
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
